81a5ccf14afa0ed30a80cc962ffb0906f0efc281
Red Penguin <redpenguin777@yahoo.com>
2024-05-05 01:29:04 +0100

    format to use tabs

--- src/rensutils.h
+++ src/rensutils.h
@@ -19,12 +19,12 @@ float deg2rad(float degrees);
 
 LCDBitmap *loadImageAtPath(PlaydateAPI *pd, const char *path);
 #define lazyLoadImageAtPath( VAR, FILE ) \
-  static LCDBitmap* VAR = NULL; \
-  if (VAR == NULL) VAR = loadImageAtPath(pd, FILE);
+	static LCDBitmap* VAR = NULL; \
+	if (VAR == NULL) VAR = loadImageAtPath(pd, FILE);
 LCDBitmapTable *loadSpritesheetAtPath(PlaydateAPI *pd, const char *path);
 #define lazyLoadSpritesheetAtPath( VAR, FILE ) \
-  static LCDBitmapTable* VAR = NULL; \
-  if (VAR == NULL) VAR = loadSpritesheetAtPath(pd, FILE);
+	static LCDBitmapTable* VAR = NULL; \
+	if (VAR == NULL) VAR = loadSpritesheetAtPath(pd, FILE);
 
 bool isPosOnScreen(int x, int y);
 
