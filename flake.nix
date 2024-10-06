a2787bb268e10d785da834e2f39e90813574ba5c
Red Penguin <redpenguin777@yahoo.com>
2024-10-06 13:01:05 +0100

    added a flake.nix for development on nixos

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+	description = "Flake for Playdate Development in Nix";
+
+	inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    playdate-sdk.url = "github:RegularTetragon/playdate-sdk-flake";
+  };
+
+  outputs = { self, nixpkgs, playdate-sdk, ... }:
+	  let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+      stdenv = pkgs.stdenv;
+      playdate-sdk-pkg = playdate-sdk.packages.${system}.default;
+    in {
+      devShells.x86_64-linux.default = pkgs.mkShell {
+        packages = [playdate-sdk-pkg];
+
+        nativeBuildInputs = with pkgs; [
+          gcc-arm-embedded
+        ];
+
+        PLAYDATE_SDK_PATH = "${playdate-sdk-pkg}";
+      };
+    };
+}
