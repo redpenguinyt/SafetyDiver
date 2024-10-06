75264974f21f3846afd4210eaac9ec4e6f29eabf
Red Penguin <redpenguin777@yahoo.com>
2024-10-06 15:41:46 +0100

    flake.nix - fmt

--- flake.nix
+++ flake.nix
@@ -6,16 +6,14 @@
     playdate-sdk.url = "github:RegularTetragon/playdate-sdk-flake";
   };
 
-  outputs = { self, nixpkgs, playdate-sdk, ... }:
+  outputs = { nixpkgs, playdate-sdk, ... }:
 	  let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
-      stdenv = pkgs.stdenv;
       playdate-sdk-pkg = playdate-sdk.packages.${system}.default;
     in {
       devShells.${system}.default = pkgs.mkShell {
-        packages = [playdate-sdk-pkg];
-        nativeBuildInputs = [ pkgs.gcc-arm-embedded ];
+        packages = [ playdate-sdk-pkg pkgs.gcc-arm-embedded ];
         PLAYDATE_SDK_PATH = "${playdate-sdk-pkg}";
       };
     };
